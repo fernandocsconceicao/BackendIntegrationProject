199706501e9be8dda2699ed1aecf30d62b3c82e1
fernandocsconceicao <58656340+fernandocsconceicao@users.noreply.github.com>
2020-12-02 17:26:41 -0300

    first commit

---
+++ src/test/java/com/ti89/feign/FeignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ti89.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
